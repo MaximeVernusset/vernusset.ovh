edf5883bd0dd6cd02d21b310c9f31b3b9bbe4f25
Maxime Vernusset <31858272+MaximeVernusset@users.noreply.github.com>
2023-04-19 14:44:10 +0200

    Create dl-protect - Uptobox links grabber.js

---
+++ tampermonkey/dl-protect - Uptobox links grabber.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         dl-protect - Uptobox links grabber
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Grab uptobox links from dl-protect.link and redirect to vernusset.ovh collector page
+// @author       Maxime Vernusset
+// @match        https://dl-protect.link/*
+// @grant        none
+// ==/UserScript==
+
+const uptoboxLinksGeneratorUrl = 'http://vernusset.ovh/index.php?action=pyload/collect';
+
+function clickSubmitButton() {
+    const button = document.getElementById('subButton');
+    if (button) {
+        button.click();
+    }
+}
+
+function accessLinks() {
+    const observer = new MutationObserver(function() {
+        //this.disconnect();
+        clickSubmitButton();
+    });
+    observer.observe(document.getElementsByTagName('body')[0], { attributes: false, childList: true });
+}
+
+function collectLinks() {
+    const links = [];
+    for (let div of document.getElementsByClassName('urls')) {
+        for (let ul of div.children) {
+            for (let li of ul.getElementsByTagName('li')) {
+                for (let a of li.getElementsByTagName('a')) {
+                    links.push(encodeURI(a.href));
+                }
+            }
+        }
+    }
+    return links;
+}
+
+function collectTitle() {
+    return document.getElementsByTagName('h3')[0].textContent;
+}
+
+(function() {
+    'use strict';
+    accessLinks();
+    const links = collectLinks();
+    const title = collectTitle();
+    if (links.length > 0) {
+        document.location=encodeURI(`${uptoboxLinksGeneratorUrl}&links=${JSON.stringify(links)}&name=${title}`);
+    }
+})();
